158f0461466d0fe7a72132199b7fd5af82f7e5fc
Giovanni Campagna <gcampagn@redhat.com>
2013-09-27 11:33:24 +0200

    Start implementing the actual application

--- src/window.js
+++ src/window.js
@@ -1,28 +1,22 @@
 // -*- Mode: js; indent-tabs-mode: nil; c-basic-offset: 4; tab-width: 4 -*-
 //
-// Copyright (c) 2013 Giovanni Campagna <scampa.giovanni@gmail.com>
+// Copyright (c) 2013 Red Hat, Inc.
 //
-// Redistribution and use in source and binary forms, with or without
-//  modification, are permitted provided that the following conditions are met:
-//   * Redistributions of source code must retain the above copyright
-//     notice, this list of conditions and the following disclaimer.
-//   * Redistributions in binary form must reproduce the above copyright
-//     notice, this list of conditions and the following disclaimer in the
-//     documentation and/or other materials provided with the distribution.
-//   * Neither the name of the GNOME Foundation nor the
-//     names of its contributors may be used to endorse or promote products
-//     derived from this software without specific prior written permission.
+// Gnome App Tools is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2 of the License, or (at your
+// option) any later version.
 //
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
-// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// Gnome App Tools is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with Gnome App Tools; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+// Author: Giovanni Campagna <scampa.giovanni@gmail.com>
 
 const GLib = imports.gi.GLib;
 const GObject = imports.gi.GObject;
@@ -51,15 +45,18 @@ const MainWindow = new Lang.Class({
         let builder = new Gtk.Builder();
         builder.add_from_resource('/org/gnome/AppTool/main.ui');
 
-        this.set_titlebar(builder.get_object('main-header'));
+        let stack = builder.get_object('main-stack');
+        this.add(stack);
 
-        let grid = builder.get_object('main-grid');
-        this.add(grid);
+        let switcher = new Gtk.StackSwitcher({ stack: stack });
+        let header = builder.get_object('main-header');
+        header.custom_title = switcher;
+        this.set_titlebar(header);
     },
 
     _about: function() {
         let aboutDialog = new Gtk.AboutDialog(
-            { authors: [ 'Giovanni Campagna <gcampagn@redhat.com>' ],
+            { authors: [ 'Giovanni Campagna <scampa.giovanni@gmail.com>' ],
               translator_credits: _("translator-credits"),
               program_name: _("Gnome Application Tool"),
               comments: _("A tool for editing Gtk+ application metadata"),
